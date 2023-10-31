15efa1c8ed350997f475549e6f3ccd5c3c4fd5d0
yoqub-dev <yoqub.epam@gmail.com>
2023-10-31 15:59:40 +0500

    task15

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -3,9 +3,17 @@ package school.mjc.stage0.loops.finalTask;
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("8");
+            // Print spaces for alignment
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print("  "); // Two spaces for alignment
             }
+
+            // Print asterisks for the pyramid
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next line for the next row
             System.out.println();
         }
     }
